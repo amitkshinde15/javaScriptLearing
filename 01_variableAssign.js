257b521d5073e4ba010b28905609ed0c331471ce
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-08 20:10:21 +0530

    variable assignment
    variable assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,21 @@
+var myFullName;
+console.log(myFullName);
+myFullName= "Amutkumar Shinde";
+console.log(myFullName);
+myFullName="Amitkumar Machindranath Shinde";
+console.log(myFullName);
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var firstName="Amitkumar";
+var lastName="Shinde";
+var age=34;
+var address="A/P Goregaon(Vangi) Tal-Khatav Dist-Satara";
+var pin_code=411512;
+var gen="Male";
+var is_married="Yes";
+
+console.log(firstName,lastName,age,address,pin_code,gen,is_married);
+
+
+
