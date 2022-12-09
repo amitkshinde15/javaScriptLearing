f6fb3eafd895bc1561fc226c5a15c65b478777f3
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-09 21:15:39 +0530

    Create 01_swapTwoNumber.js

---
+++ 01_swapTwoNumber.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+console.log("Before Swaping two number: " +a,b);
+// var temp=a;
+// a=b;
+// b=temp;
+// console.log("After Swaping: " +a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+console.log("After Swaping: " +a,b);
+
