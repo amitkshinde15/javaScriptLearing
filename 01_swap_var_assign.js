3f85483cef15b9db9f1d2e42603ee393e4cd3cce
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-09 21:15:28 +0530

    javaScript program
    program

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,21 @@
+var sweety="Sweeety";
+var cutie="Cutie";
+console.log("-----------Before swap-------------");
+console.log(sweety, cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("-----------After swap--------------");
+console.log(sweety,cutie);
+console.log("===================================");
+var num1=100, num2=200, num3=300;
+console.log("-----------Before swap-------------");
+console.log(num1,num2,num3);
+console.log("-----------After swap--------------");
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("-----------Before swap-------------");
+console.log(num1,num2,num3);
+
---
+++ 01_typeOf.js
@@ -0,0 +1,24 @@
+var bankName;
+console.log("Before Initialization value of variable is" +" - " +bankName);
+bankName="SBI Bank";
+console.log("After Initialization value of variable is" +" - "+bankName);
+console.log("DataType of variable bankName is" +" - "+typeof bankName);
+console.log("----------------------------------------------------");
+var marks='90%';
+console.log("DataType of variable marks is "+" - "+ typeof marks);
+console.log("----------------------------------------------------");
+var isWorking;
+console.log("Before Initialization value of variable is" +" - " +isWorking);
+isWorking=true;
+console.log("After Initialization value of variable is" +" - "+isWorking);
+console.log("DataType of variable isWorking is "+" - "+ typeof isWorking);
+console.log("----------------------------------------------------");
+var totalCount="Hundred and Seven";
+console.log("DataType of variable is "+ typeof totalCount +" and It's value is "+ totalCount);
+totalCount=107;
+console.log("DataType of variable is "+ typeof totalCount +" and It's value is "+ totalCount);
+
+
+
+
+
