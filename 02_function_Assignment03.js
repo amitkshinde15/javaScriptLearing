880ea3813fe4be60d81a572e467e5177ec793f7d
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-14 21:15:56 +0530

    Update 02_function_Assignment03.js

--- 02_function_Assignment03.js
+++ 02_function_Assignment03.js
@@ -3,7 +3,7 @@ var simpleFun = function squareOfWordLength(value1)
     var len=value1.length;
     console.log(`Length of word "${value1}" is "${len}" and square of length  is "${len*len}"`);
 }
-simpleFun("javaScript");
+simpleFun("JavaScript");
 simpleFun("Google Chrome");
 simpleFun("Developer Smart");
 console.log("----------------------------------------------------------------------");
@@ -12,7 +12,7 @@ var name1 = function(){
     var string1= "I am Angular Developer";
     var stringlen1=string1.length;
     var totalWord=string1.split(" ").length;
-    console.log(`Length of a given string is "${stringlen1}" and no. of words are "${totalWord}"\n`);
+    console.log(`Length of "I am Angular Developer" string is "${stringlen1}" and no. of words are "${totalWord}"\n`);
     var result=stringlen1/totalWord;
     console.log(`After dividing length by word result = "${result}"`);
     console.log(`String length multiply by word = "${stringlen1*totalWord}"`);
