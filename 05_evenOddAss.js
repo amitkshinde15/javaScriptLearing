8f590a872c2e565f2e658aff7b8c8741942f1361
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-19 22:16:34 +0530

    Assignment
    Assignment

--- 05_evenOddAss.js
+++ 05_evenOddAss.js
@@ -1,18 +1,18 @@
-var evenOrodd = function(value)
-{
-    if(value%2==0)
-    {
-        console.log(`Given number ${value} is "Even" number`);
+function isEvenOrOdd(num) {
+    if (typeof num == "number") {
+      console.log(`Valid number ${num}`);
+      if (num % 2 === 0) {
+        console.log(`Even number`);
+      } else {
+        console.log(`Odd Number`);
+      }
+    } else {
+      console.log(`Invalid number ${num}`);
     }
-    else{
-        console.log(`Given number ${value} is "Odd" number`);
-    }
-    
-}
-evenOrodd(45);
-evenOrodd(70);
-evenOrodd(67);
-evenOrodd(98);
+  }
+  isEvenOrOdd(20);
+  isEvenOrOdd(11);
+  isEvenOrOdd("30");
 console.log("--------------------------------------------------------------");
 function vote(name,value)
 {
--- 05_ifStatementAss.js
+++ 05_ifStatementAss.js
@@ -1,48 +1,52 @@
-function age(value){
-    if(value>=18 && value<=120)
-    {
-        console.log(`Your age is "${value}" and you are eligible for voting`);
+function age(value) {
+  if (typeof value == "number") {
+    if(value<=0 || value>120){
+        console.log(`You have entered "${value}" this is invalid data`);
     }
-    else if(value<18 && value>0)
-    {
-        console.log(`Your age is "${value}" and you are not eligible for voting`);
+    if (value >= 18 && value <= 120) {
+      console.log(`Your age is "${value}" and you are eligible for voting`);
     }
-    else
-    {
-        console.log(`You have entered "${value}" this is invalid data`); 
+    if (value < 18 && value > 0) {
+      console.log(`Your age is "${value}" and you are not eligible for voting`);
     }
+  } else {
+    console.log(`You have entered string value "${value}" this is invalid data`);
+  }
 }
-age(45);
+age("45");
 age(17);
 age(8);
 age(20);
 age(-10);
 age(200);
 age(0);
-console.log("==============================================================================");
-function gradCalculation(marks)
-{
-    if(marks>=90 && marks<=100){
-        console.log(`Funtastic marks: "${marks}", your grade is A+`);
-    }
-    else if(marks>=70 && marks<90){
-        console.log(`Excellent marks "${marks}", your grade is A`);
-    }
-    else if(marks>=50 && marks<75){
-        console.log(`Good marks "${marks}", your grade is B`);
-    }
-    else if(marks>=35 && marks<50){
-        console.log(`Marks is "${marks}", your grade is C, Need to improvement`);
-    }
-    else if(marks>0 && marks<35){
-        
-        console.log(`Marks is "${marks}", Unfortunately you are fail, Please try next time`);
-    }
-    else{
-        console.log(`You have entered "${marks}", Please provide the valid marks`);
-    }
+console.log(
+  "=============================================================================="
+);
+function gradCalculation(marks) {
+    if(typeof marks=="number"){
+        if(marks<=0 || marks>100)
+        {
+            console.log(`You have entered "${marks}", Please provide the valid marks`);
+        } 
+  if (marks >= 90 && marks <= 100) {
+    console.log(`Funtastic marks: "${marks}", your grade is A+`);
+  } if (marks >= 70 && marks < 90) {
+    console.log(`Excellent marks "${marks}", your grade is A`);
+  } if (marks >= 50 && marks < 75) {
+    console.log(`Good marks "${marks}", your grade is B`);
+  } if (marks >= 35 && marks < 50) {
+    console.log(`Marks is "${marks}", your grade is C, Need to improvement`);
+  } if (marks > 0 && marks < 35) {
+    console.log(
+      `Marks is "${marks}", Unfortunately you are fail, Please try next time`
+    );
 }
-gradCalculation(98);
+  } else {
+    console.log(`You have entered string value "${marks}", Please provide the valid marks`);
+  }
+}
+gradCalculation("98");
 gradCalculation(80);
 gradCalculation(90);
 gradCalculation(0);
@@ -52,7 +56,3 @@ gradCalculation(35);
 gradCalculation(29);
 gradCalculation(64);
 gradCalculation(49);
-
-
-
-
