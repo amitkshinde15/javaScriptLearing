4243ff7d145831dc3fcb31ae43913ae5ead9fb59
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-27 21:39:44 +0530

    assignment
    assignment

--- 08_objectAssignment_01.js
+++ 08_objectAssignment_01.js
@@ -1,28 +1,28 @@
 const professor = {
-    name : "Rahul Dravid",
-    hight : 5.7,
-    weight: 75,
-    age: 37,
-    subject : "Maths",
-    
+  name: "Rahul Dravid",
+  hight: 5.7,
+  weight: 75,
+  age: 37,
+  subject: "Maths",
 
-    degrees : {
-        engineering : "CSC",
-        PHD : "Adv Computing",
-        MS : "JavaScript",
-    },
+  degrees: {
+    engineering: "CSC",
+    PHD: "Adv Computing",
+    MS: "JavaScript",
+  },
 
-    certificates : {
-
-        first : "Hacker Rank Participation",
-        second : "Certificate in IFE course",
-        third : "Certificate in Adv programming",
-    },
-    value : function() {
-     return professor.degrees.engineering+" "+professor.degrees.PHD+" "+professor.degrees.MS; 
-    },
-    
-}
+  certificates: {
+    first: "Hacker Rank Participation",
+    second: "Certificate in IFE course",
+    third: "Certificate in Adv programming",
+  },
+  value: function () {
+    return (
+      professor.degrees.engineering.concat(" ",professor.degrees.PHD, " ",professor.degrees.MS)
+      
+    );
+  },
+};
 console.log(`01. Professor Information`);
 console.log(professor);
 console.log(`02. Adding nested object degree`);
@@ -33,7 +33,9 @@ const totalDegree = professor.value();
 console.log(`04. Total degrees are: ${totalDegree}`);
 
 professor.city = "Pune";
-console.log(`05. Added new property in object professor is city name ${professor.city}`);
+console.log(
+  `05. Added new property in object professor is city name ${professor.city}`
+);
 console.log(professor);
 professor.subject = "JavaScript Programming";
 console.log(`06. Modifying existing property subject`);
@@ -44,6 +46,3 @@ console.log(professor.certificates);
 console.log(`08. After adding new certificate`);
 professor.certificates.third = "Advance java";
 console.log(professor.certificates);
-
-
-
--- 08_objectAssignment_02.js
+++ 08_objectAssignment_02.js
@@ -1,39 +1,49 @@
-const sbiBank ={
-    bankName : "SBI Bank",
-    location : "Mumbai",
-    accountNo : "41523564581",
-    ifsc : "SBIN000065",
-    interestRate : 7.5,
-}
-const axisBank = {
-    bankName : "AXIS Bank",
-    location : "Pune",
-    accountNo : "77523564456",
-    ifsc : "AXI001276",
-    interestRate : 8.5,
-}
-const hdfcBank = {
-    bankName : "HDFC Bank",
-    location : "Kolhapur",
-    accountNo : "78952458741",
-    ifsc : "HDFC004568",
-    interestRate : 7.8,
-}
-const yesBank = {
-    bankName : "SBI Bank",
-    location : "Mumbai",
-    accountNo : "41523564581",
-    ifsc : "SBIN000065",
-    interestRate : 7.9,
-}
-function showDetails(){
+const sbiBank = {
+  bankName: "SBI Bank",
+  location: "Mumbai",
+  accountNo: "41523564581",
+  ifsc: "SBIN000065",
+  interestRate: 7.5,
+  showDetails: function () {
     console.log(`Invoke SBI Bank details using function`);
     console.log(sbiBank);
+  },
+};
+const axisBank = {
+  bankName: "AXIS Bank",
+  location: "Pune",
+  accountNo: "77523564456",
+  ifsc: "AXI001276",
+  interestRate: 8.5,
+  showDetails: function () {
     console.log(`Invoke AXIS Bank details using function`);
     console.log(axisBank);
+  },
+};
+const hdfcBank = {
+  bankName: "HDFC Bank",
+  location: "Kolhapur",
+  accountNo: "78952458741",
+  ifsc: "HDFC004568",
+  interestRate: 7.8,
+  showDetails: function () {
     console.log(`Invoke HDFC Bank details using function`);
     console.log(hdfcBank);
-    console.log(`Invoke YES Bank details using function`);
-    console.log(yesBank);
-}
-showDetails();
+  },
+};
+const yesBank = {
+  bankName: "SBI Bank",
+  location: "Mumbai",
+  accountNo: "41523564581",
+  ifsc: "SBIN000065",
+  interestRate: 7.9,
+  showDetails: function () {
+    console.log(`Invoke SBI Bank details using function`);
+    console.log(sbiBank);
+  },
+};
+sbiBank.showDetails();
+axisBank.showDetails();
+hdfcBank.showDetails();
+yesBank.showDetails();
+
