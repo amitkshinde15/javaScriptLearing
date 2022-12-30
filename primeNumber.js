93b780836e683b2bf3556af632f4fb8fa45adec4
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-30 21:25:38 +0530

    Update primeNumber.js

--- primeNumber.js
+++ primeNumber.js
@@ -1,18 +1,21 @@
 function prime(number) {
-  if (number == 0 || number == 1) {
-    console.log(`Given "${number}" is not a prime number`);
+  var isPrime = true;
+  if (number <= 1) {
+    console.log(`Given number ${number} is not prime number`);
   } else {
     for (let i = 2; i < number; i++) {
-      if (number % i == 0) {
-        console.log(`Given "${number}" is not a prime number`);
-        break;
-      } else {
-        console.log(`Given "${number}" is a prime number`);
-        break;
+      if (number % 2 == 0) {
+        isPrime = false;
       }
+    }
+    if (isPrime) {
+      console.log(`Given number ${number} is prime number`);
+    } else {
+      console.log(`Given number ${number} is not prime number`);
     }
   }
 }
 prime(11);
 prime(18);
+prime(2);
 prime(0);
