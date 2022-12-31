9043ce016565f3585889115fcfd5c5b43775a492
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-31 21:12:50 +0530

    Update palindrom.js

--- palindrom.js
+++ palindrom.js
@@ -1,28 +1,33 @@
 console.log("--------Validating given string is palindrome or not--------");
-function palindrome(str){
-    var reverse = "";
-    for(let i=str.length-1;i>=0;i--){
-        reverse = reverse + str.charAt(i);
-    }
-    console.log(`Given string is ${str}`);
-    console.log(`Reverse string is ${reverse}`);
-    if(str==reverse){
-        console.log(`Given string is Palindrome`);
-    }
-    else
-    console.log(`Given string is not Palindrome`);
+function palindrome(str) {
+  var reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse = reverse + str.charAt(i);
+  }
+  console.log(`Given string is ${str}`);
+  console.log(`Reverse string is ${reverse}`);
+  if (str == reverse) {
+    console.log(`Given string is Palindrome`);
+  } else console.log(`Given string is not Palindrome`);
 }
 palindrome("amit");
 palindrome("anna");
 
 console.log("--------Validating given number is palindrome or not--------");
-function palnum(num){
-    var reverse =0;
-    while (num>0) {
-        var remainder = num%10;
-        reverse = (reverse*10) + remainder;
-        num=parseInt(num/10);
-    }
-    console.log(reverse);
+function palnum(num) {
+  let temp;
+  let reverse = 0;
+  temp = num;
+  while (num > 0) {
+    var remainder = num % 10;
+    num = parseInt(num / 10);
+    reverse = reverse * 10 + remainder;
+  }
+  if (temp == reverse) {
+    console.log(`Given number ${temp} = ${reverse} is palindrome number`);
+  } else {
+    console.log(`Given number ${temp} != ${reverse} is not palindrome number`);
+  }
 }
-palnum(153);              
+palnum(153);
+palnum(535);
