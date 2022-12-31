649b2ed4c51530826a840af5206d7db9a8efe494
amitkshinde15 <amitkshinde15@gmail.com>
2022-12-31 22:18:04 +0530

    changes

--- fibonacci.js
+++ fibonacci.js
@@ -12,23 +12,4 @@ for (let index = 0  ; index <=num; index++) {
 }
 console.log(`---------------01.Fibonacci Series----------------`);
 console.log(`${array}`);
-console.log(`---------------02.Prime number-------------------`);
-
-function prime(number) {
-    if (number == 0 || number == 1) {
-      console.log(`Given number "${number}" is not a prime number`);
-    } else {
-      for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-          console.log(`Given number "${number}" is not a prime number`);
-          break;
-        } else {
-          console.log(`Given number"${number}" is a prime number`);
-          break;
-        }
-      }
-    }
-  }
-prime(11);
-prime(18);
-prime(0);
+console.log(array);
